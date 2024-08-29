4bdc0188f3f91164cabbe9877007a3f504f92a66
Thomas Blake <tb69613@email.vccs.edu>
2024-08-29 15:03:58 -0400

    PointType updated

--- CSC223ReviewProject.cpp
+++ CSC223ReviewProject.cpp
@@ -1,10 +1,36 @@
 // CSC 223 Review project, there are three branches. PointType Class, CircleType Class, & CylinderType Class.
+// This tests pointtype class.
 
 #include <iostream>
+#include <iomanip>
+#include "PointType.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "CSC 223 Review Project" << endl;
+    cout << "From default constructor:" << endl;
+    PointType<int> p1I;
+    PointType<double> p1D;
+    cout << "Integer point: ";
+    p1I.print();
+    cout << "Double point: ";
+    p1D.print();
+
+    cout << "\nConstructor with parameters:" << endl;
+    PointType<int> p2I(3, 5);
+    PointType<double> p2D(3.5, 5.5);
+    cout << "Integer point: ";
+    p2I.print();
+    cout << "Double point: ";
+    p2D.print();
+
+    cout << "\n(integer) Distance = ";
+    double distance = p1I - p2I;
+    cout << distance << endl;
+
+    cout << "\n(double) Distance = ";
+    distance = p1D - p2D;
+    cout << distance << endl;
+
 }
---
+++ PointType.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+using namespace std;
+template <class T>
+class PointType
+{
+private:
+	T x;
+	T y;
+
+public:
+	//Default constructor
+	PointType() { x = 0, y = 0; cout << fixed << setprecision(1) << showpoint;
+	}
+
+	//Constructor with parameters
+	PointType(T Ux, T Uy) : x(Ux), y(Uy) { cout << fixed << setprecision(1) << showpoint; }
+
+	//Setters
+	void setX(T Ux) { x = Ux; }
+	void setY(T Uy) { y = Uy; }
+	void setPoint(T Ux, T Uy) { x = Ux; y = Uy; }
+
+	//Getters
+	T getX() { return x; }
+	T getY() { return y; }
+	void getPoint(T& Ux, T& Uy) { Ux = x; Uy = y; }
+
+
+	double operator -(PointType<T> op2) {
+		double distance = sqrt(pow(x - op2.x, 2) + pow(y - op2.y, 2));
+		return distance;
+	}
+
+	void print() {
+		cout << "(" << x << ", " << y  << ")" << endl;
+	}
+};
+
